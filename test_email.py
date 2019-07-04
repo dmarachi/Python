b5414fa8b9b06de7b5964bb450a07d2911b193b3
dmarachi <48455044+dmarachi@users.noreply.github.com>
2019-07-04 15:03:29 -0600

    Updating the test email

--- test_email.py
+++ test_email.py
@@ -1,14 +1,15 @@
 import unittest
-import email
+import email1
 
 class emailTests(unittest.TestCase):
 
 	def test_email_method(self):
-		self.assertEqual(email.email("larry", "shumlich"), "larry.shumlich@evolveu.ca")
+		self.assertEqual(email1.email("larry", "shumlich"), "larry.shumlich@evolveu.ca")
+		self.assertEqual(email1.email("dima", "marachi"), "dima.marachi@evolveu.ca")
 
-	def test_email_method():
-    assert my_email("larry", "shumlich") == "larry.shumlich@evolveu.ca"
-    assert my_email("heiko", "peters") == "heiko.peters@evolveu.ca"
+	# def test_email_method():
+	# 	assert my_email("larry", "shumlich") == "larry.shumlich@evolveu.ca"
+	# 	assert my_email("heiko", "peters") == "heiko.peters@evolveu.ca"
 
     
 if __name__ == "__main__":
